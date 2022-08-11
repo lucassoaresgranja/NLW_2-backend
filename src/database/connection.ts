7af11498f88d1cdf981babf9e0ce67c5643984ac
Lucas Soares Granja <lucassoaresgranja@yahoo.com.br>
2022-08-11 17:00:48 -0300

    Atualizando

--- src/database/connection.ts
+++ src/database/connection.ts
@@ -6,7 +6,7 @@ import path from 'path';
 const db = knex({
     client: 'sqlite3',
     connection:{
-        filename: path.resolve(__dirname,'connection','database.sqlite')
+        filename: path.resolve(__dirname,'database.sqlite')
     },
     useNullAsDefault: true,
 });
