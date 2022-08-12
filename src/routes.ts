fec4cee015bca940fa095ccebba9c44a1b82e972
Lucas Soares Granja <lucassoaresgranja@yahoo.com.br>
2022-08-11 23:03:45 -0300

    Finalização

--- src/routes.ts
+++ src/routes.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import ClassesController from './controllers/ClassesController';
+import ConnectionController from './controllers/ConnectionsController';
 
 
 
@@ -9,8 +10,13 @@ const routes = express.Router();
 
 const classesController = new ClassesController();
 
+const connectionsController = new ConnectionController();
 
+routes.get('/classes',classesController.index);
 routes.post('/classes',classesController.create);
+routes.post('/connections',connectionsController.create);
+routes.get('/connections',connectionsController.index);
+
 
 
 export default routes;
